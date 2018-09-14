bf0d196e9899ef7b8457e66f13210cfc43927feb
feiYu2 <1726260651@qq.com>
2018-09-13 18:57:51 -0700

    firstcom

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: Mon 10 Sep 2018 06:42:16 PM PDT
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+   
+int main()
+{
+      printf("hello.world!\n");
+      return 0;
+}
